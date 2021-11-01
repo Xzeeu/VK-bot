0470e5af40bd1813c5c26660b22679eab36291c5
Pavel <kat.pasha0@gmsil.com>
2021-11-01 14:41:43 +0500

    fix time lesson

--- rasp.py
+++ rasp.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+'''from datetime import datetime
 from timelessons import*
 import time
 from knopki import now
@@ -83,3 +83,4 @@ for i in dku_0:
         dku += i
 
 
+'''
