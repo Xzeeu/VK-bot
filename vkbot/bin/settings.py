5d28a7321350e8506d993ab2fa31cd7a3ae632fc
Pavel Kataytsev <73869416+Xzeeu@users.noreply.github.com>
2022-03-25 18:28:35 +0500

    Update settings.py

--- vkbot/bin/settings.py
+++ vkbot/bin/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 vk_session = vk_api.VkApi(token=S3Connection(os.environ['GROUP_TOKEN'], os.environ['GROUP_TOKEN']))
 
 
-GROUP_ID = S3Connection(os.environ['GROUP_ID'], os.environ['GROUP_ID'])
+GROUP_ID = '207758161'
 
 GROUP_TOKEN = S3Connection(os.environ['GROUP_TOKEN'], os.environ['GROUP_TOKEN'])
 
@@ -24,7 +24,7 @@ CALLBACK_TYPES = ('show_snackbar', 'open_link', 'open_app')
 
 vk_session = VkApi(token=GROUP_TOKEN, api_version=API_VERSION)
 vk_ = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, 207758161)
+longpoll = VkBotLongPoll(vk_session, group_id=GROUP_ID)
 
 
 
