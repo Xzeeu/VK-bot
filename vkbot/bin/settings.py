ee833736c2fbf1c6be83b560d395d2b6c7526b9d
Pavel Kataytsev <73869416+Xzeeu@users.noreply.github.com>
2022-03-25 20:55:16 +0500

    Update settings.py

--- vkbot/bin/settings.py
+++ vkbot/bin/settings.py
@@ -11,12 +11,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-vk_session = vk_api.VkApi(token=S3Connection(os.environ['GROUP_TOKEN'], os.environ['GROUP_TOKEN']))
+vk_session = vk_api.VkApi(token=S3Connection(os.environ['GROUP_TOKEN']))
 
 
 GROUP_ID = '207758161'
 
-GROUP_TOKEN = S3Connection(os.environ['GROUP_TOKEN'], os.environ['GROUP_TOKEN'])
+GROUP_TOKEN = S3Connection(os.environ['GROUP_TOKEN'])
 
 
 API_VERSION = '5.120'
